ef48075a095c60bd65a80fd1761f22d449715859
coldbloodx <naughtyboywx@yeah.net>
2015-08-15 02:00:09 +0800

    update weight sep dlg

--- src/DialogClasses/implementer/FormulaSepWeighDialog.cpp
+++ src/DialogClasses/implementer/FormulaSepWeighDialog.cpp
@@ -160,8 +160,8 @@ BOOL CFormulaSepWeighDialog::OnInitDialog()
 
 	m_Com1Value.SetFont(SingletonHelper::getInstance()->simHei40, TRUE);
 	m_Com2Value.SetFont(SingletonHelper::getInstance()->simHei40, TRUE);
-    uiFunctions::setdlgsize(this);
-
+    uiFunctions::setdlgsize(this, &m_CancalButton, &m_ButtonOK);
+    
 	return TRUE;  
 }
 
